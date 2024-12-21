566fa0a124b91c96c736adc74fe07593a3d41608
Thipiga Raj <thipiga58@gmail.com>
2024-12-20 20:47:30 -0500

    DB update commit 2

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.createConnection('mongodb+srv://thipiga57:<db_password>@cluster0.rev29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+const connection = mongoose.createConnection('mongodb+srv://thipiga57:KG65WmscA6ToE2Io@cluster0.rev29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 connection.on('connected', () => {
     console.log('DB Connected');
