ec0a8f9d0b46e32d0eb8554976acd41e81ef0d89
Thipiga Raj <thipiga58@gmail.com>
2024-12-20 22:29:27 -0500

    DB update commit 5

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.createConnection('mongodb+srv://user1:abc123d4@cluster0.rev29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+const connection = mongoose.createConnection('mongodb+srv://user1:abc123d4@cluster0.rev29.mongodb.net/mobile_app?retryWrites=true&w=majority&appName=Cluster0');
 
 connection.on('connected', () => {
     console.log('DB Connected');
